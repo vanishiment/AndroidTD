df9582ff5d6de05e6f4985b76a6035fcc38d3919
vanishiment <1362137760@qq.com>
2017-10-15 22:22:37 +0800

    插件化-插件模块-plugin

---
+++ pluginlib/src/main/java/com/plugin/lib/PluginService.java
@@ -0,0 +1,18 @@
+package com.plugin.lib;
+
+import android.app.Service;
+import android.content.Intent;
+import android.content.res.Resources;
+
+public class PluginService {
+
+    private static final String TAG = "PluginService";
+
+    public void setProxy(Service hostService, Resources hostRes){
+
+    }
+
+    public void onStartCommand(Intent intent, int flags, int startId) {
+
+    }
+}
