82b5bd1449034c9db762a81a3c502b464c3ea771
vanishiment <1362137760@qq.com>
2017-10-15 16:37:31 +0800

    插件化-加载模块-ProxyService

--- loaderlib/src/main/java/com/loader/lib/PluginLoader.java
+++ loaderlib/src/main/java/com/loader/lib/PluginLoader.java
@@ -1,5 +1,13 @@
 package com.loader.lib;
 
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
+import android.text.TextUtils;
+import android.util.Log;
+
+import java.io.File;
+
 import dalvik.system.DexClassLoader;
 
 public class PluginLoader {
@@ -8,39 +16,101 @@ public class PluginLoader {
 
     private DexLoader mDexLoader;
     private DexClassLoader mDCL;
+    private ClassLoader mContextClassLoader;
+    private String zipPath;
+    private String dexPath;
 
-    public PluginLoader() {
+    private Handler mMainHandler = new Handler(Looper.getMainLooper());
+
+    public PluginLoader(Context context) {
         mDexLoader = DexLoader.getInstance();
         mDCL = mDexLoader.getDexClassLoader();
+        mContextClassLoader = context.getClassLoader();
+        zipPath = context.getCacheDir().getAbsolutePath() + File.separator + "zip";
+        dexPath = context.getCacheDir().getAbsolutePath() + File.separator + "dex";
     }
 
-    public synchronized void loadClass(String className,OnClassLoadListener loadListener){
-        if (mDCL == null){
-            if (!mDexLoader.isIsLoading()){
+    public synchronized void loadClass(final String className, final OnClassLoadListener loadListener) {
+        if (mDCL == null) {
+            if (!mDexLoader.isIsLoading()) {
                 mDexLoader.setIsLoading(true);
-                // TODO: 2017/10/14 开始加载 zip
-            }else {
-                // TODO: 2017/10/14 延时 loadClass
+                new LoaderTask().execute(className,loadListener,zipPath,dexPath);
+            } else {
+                mMainHandler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        loadClass(className, loadListener);
+                    }
+                }, 1000);
             }
-        }else {
-            if (!hasUpdateZip()){
-                Class<?> clazz;
+        } else {
+            Class<?> clazz;
+            try {
+                clazz = mDCL.loadClass(className);
+                loadListener.onLoadClass(true, clazz);
+            } catch (ClassNotFoundException e) {
                 try {
-                    clazz = mDCL.loadClass(className);
-                    loadListener.onLoadClass(true,clazz);
-                } catch (ClassNotFoundException e) {
-                    // TODO: 2017/10/14 使用 Context 的 ClassLoader 加载 class
+                    clazz = mContextClassLoader.loadClass(className);
+                    if (clazz != null){
+                        loadListener.onLoadClass(true,clazz);
+                    }else {
+                        loadListener.onLoadClass(false,null);
+                    }
+                } catch (ClassNotFoundException e1) {
+                    loadListener.onLoadClass(false,null);
                 }
-            }else {
-                mDCL = null;
-                loadClass(className, loadListener);
             }
         }
     }
 
-    private boolean hasUpdateZip(){
-        return false;
+    private class LoaderTask extends com.loader.lib.AsyncTask<Object,Void,DexClassLoader>{
+
+        private String className;
+        private OnClassLoadListener listener;
+        String zipPath;
+        String dexOutputPath;
+
+        @Override
+        protected DexClassLoader doInBackground(Object... params) {
+            className = (String) params[0];
+            listener = (OnClassLoadListener) params[1];
+            zipPath = (String) params[2];
+            dexPath = (String) params[3];
+            if (!TextUtils.isEmpty(zipPath)){
+                mDexLoader.setDexPath(zipPath);
+                try {
+                    mDCL = new DexClassLoader(zipPath,dexOutputPath,null,ClassLoader.getSystemClassLoader());
+                }catch (Exception e){
+                    Log.e(TAG, "class load error:" + e.getLocalizedMessage() );
+                    File zipFile = new File(zipPath);
+                    if (zipFile.exists() && zipFile.isFile()){
+                        zipFile.delete();
+                    }
+                }
+            }
+            return mDCL;
+        }
+
+        @Override
+        protected void onPostExecute(DexClassLoader dexClassLoader) {
+            super.onPostExecute(dexClassLoader);
+            mDexLoader.setIsLoading(false);
+            if (dexClassLoader != null){
+                mDexLoader.setDexClassLoader(dexClassLoader);
+
+                try {
+                    Class<?> clazz = dexClassLoader.loadClass(className);
+                    if (clazz != null){
+                        listener.onLoadClass(true,clazz);
+                    }else {
+                        listener.onLoadClass(false,null);
+                    }
+                } catch (ClassNotFoundException e) {
+                    listener.onLoadClass(false,null);
+                }
+            }else {
+                listener.onLoadClass(false,null);
+            }
+        }
     }
-
-
 }
--- loaderlib/src/main/java/com/loader/lib/ProxyService.java
+++ loaderlib/src/main/java/com/loader/lib/ProxyService.java
@@ -5,13 +5,19 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
 import android.util.Log;
 
 import com.loader.lib.utils.ReflectUtil;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
 public class ProxyService extends Service {
 
     private static final String TAG = "ProxyService";
+
+    private static final String TARGET_CLASS = "com.plugin.lib.PluginService";
 
     private Resources mResource;
 
@@ -30,9 +36,36 @@ public class ProxyService extends Service {
         }
     }
 
-    void launchTargetService(String className,Intent intent,int flags,int startId){
+    void launchTargetService(String className, final Intent intent, final int flags, final int startId){
         if (mRemoteService == null){
+            final PluginLoader pluginLoader = new PluginLoader(getApplicationContext());
+            pluginLoader.loadClass(className, new OnClassLoadListener() {
+                @Override
+                public void onLoadClass(boolean result, Class<?> clazz) {
+                    if (result){
+                        loadResource(DexLoader.getInstance().getDexPath());
+                        try {
+                            Constructor<?> pluginClzCons = clazz.getConstructor();
+                            Object instance = pluginClzCons.newInstance();
+                            setRemoteService(instance);
 
+                            Method setProxy = clazz.getMethod("setProxy", Service.class,Resources.class);
+                            setProxy.setAccessible(true);
+                            setProxy.invoke(instance, ProxyService.this,mResource);
+
+                            Method onStartCommand = clazz.getDeclaredMethod("onStartCommand", Intent.class,int.class,int.class);
+                            onStartCommand.setAccessible(true);
+                            onStartCommand.invoke(instance, intent,flags,startId);
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            stopSelf();
+                        }
+                    }else {
+                        stopSelf();
+                    }
+                }
+            });
         }else {
             mRemoteService.onStartCommand(intent,flags,startId);
         }
@@ -56,6 +89,18 @@ public class ProxyService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        int from;
+        if (intent != null){
+            from = intent.getIntExtra("from",0);
+            if (from == 0){
+                launchTargetService(TARGET_CLASS,intent,flags,startId);
+            }else {
+                String serviceName = intent.getStringExtra("extra_class");
+                if (!TextUtils.isEmpty(serviceName)){
+                    launchTargetService(serviceName,intent,flags,startId);
+                }
+            }
+        }
         return super.onStartCommand(intent, flags, startId);
     }
 
